eef0b4eae29f8a871992c20cd940c7e9bb2c8941
HubblersPast <90597290+HubblersPast@users.noreply.github.com>
2023-12-05 00:20:15 +0100

    fix error

--- localneed/src/ProductDetail.js
+++ localneed/src/ProductDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import products from './products';
 
 function ProductDetail({ match }) {
 	// Hier könnten Sie Logik hinzufügen, um Produktdetails basierend auf der ID zu laden
---
+++ localneed/src/products.js
@@ -0,0 +1,6 @@
+const products = [
+	{ id: 1, name: 'Brötchen', price: 0.3 },
+	{ id: 2, name: 'Kaffee', price: 2.5 }
+];
+
+export default products;
